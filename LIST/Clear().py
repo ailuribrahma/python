986c3309de6b7f4d4be17165eb244bf0db4b3140
BRAHMA <areddy685@gmail.com>
2025-10-22 03:20:15 +0530

    added iteration and clear classes

---
+++ LIST/Clear().py
@@ -0,0 +1,6 @@
+my_list = [10, 20, 30, 40, 50]
+length_of_list = len(my_list)
+print("Length of the list before clearing is:", length_of_list)
+my_list.clear()
+length_of_list_after_clear = len(my_list)
+print("Length of the list after clearing is:", length_of_list_after_clear)  
---
+++ LIST/Iterate_Through_range().py
@@ -0,0 +1,3 @@
+my_list = ['apple', 'banana', 'cherry']
+for item in range(len(my_list)):
+    print(f"Item at index {item} is {my_list[item]}")
