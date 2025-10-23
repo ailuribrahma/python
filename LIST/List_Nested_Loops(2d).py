079fb607f7c51e1a50283aa31dd5563d3284c877
BRAHMA <areddy685@gmail.com>
2025-10-23 11:03:21 +0530

    added list files

--- LIST/List_Nested_Loops(2d).py
+++ LIST/List_Nested_Loops(2d).py
@@ -1,6 +1,7 @@
-rows = 4
+rows = 5
 cols = 5
-zero_matrix = [[0 for _ in range(cols)] for _ in range(rows)]
+
+zero_matrix = [[0 for _ in range(cols)],[2 for _ in range(rows)]]
 print("2D List (Matrix) initialized with zeros:")
 for row in zero_matrix:
     print(row)  
